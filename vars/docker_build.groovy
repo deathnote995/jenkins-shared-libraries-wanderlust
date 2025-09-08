c636a84f5abd2f06004685662d0d3b312f7b7a19
Rajat Chowdhury <91901355+deathnote995@users.noreply.github.com>
2025-09-08 18:13:12 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String dockerhubuser, String Project, String ImageTag){
+def call(String Project, String ImageTag, String dockerhubuser){
   sh "docker build -t ${dockerhubuser}/${Project}:${ImageTag} ."
 }
