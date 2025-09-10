599e228924071e56a5e0d0a16f47b24d7e48b24a
Rajat Chowdhury <91901355+deathnote995@users.noreply.github.com>
2025-09-10 13:13:03 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,7 @@
 def call(){
   dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml', stopBuildOnFailure: false
+  // dependencyCheckPublisher pattern: '**/dependency-check-report.xml', stopBuildOnFailure: false
+    catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
+        dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+    }
 }
