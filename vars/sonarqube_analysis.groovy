00ca5f95b443f554c4393693b24f5bdbcda370cb
Rajat Chowdhury <91901355+deathnote995@users.noreply.github.com>
2025-09-10 18:24:10 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,18 +1,18 @@
-// def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-//   withSonarQubeEnv("${SonarQubeAPI}"){
-//      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-//   }
-// }
-def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
-  withSonarQubeEnv("${SonarQubeAPI}") {
-    def scannerHome = tool 'Sonar'  // matches the name from your screenshot
-    sh """
-      ${scannerHome}/bin/sonar-scanner \
-        -Dsonar.projectName=${Projectname} \
-        -Dsonar.projectKey=${ProjectKey} \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_AUTH_TOKEN \
-        -X
-    """
+def call(String SonarQubeAPI, String Projectname, String ProjectKey){
+  withSonarQubeEnv("${SonarQubeAPI}"){
+     sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
   }
 }
+// def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
+//   withSonarQubeEnv("${SonarQubeAPI}") {
+//     def scannerHome = tool 'Sonar'  // matches the name from your screenshot
+//     sh """
+//       ${scannerHome}/bin/sonar-scanner \
+//         -Dsonar.projectName=${Projectname} \
+//         -Dsonar.projectKey=${ProjectKey} \
+//         -Dsonar.host.url=$SONAR_HOST_URL \
+//         -Dsonar.login=$SONAR_AUTH_TOKEN \
+//         -X
+//     """
+//   }
+// }
