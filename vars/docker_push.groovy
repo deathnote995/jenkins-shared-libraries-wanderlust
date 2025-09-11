a7b9dd42b93a58ce9a7e3830be07cfed521d2f60
Rajat Chowdhury <91901355+deathnote995@users.noreply.github.com>
2025-09-11 15:00:20 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-   echo "About to log in with credentialId=dockerHubCredential for user ${dockerhubuser}"
+   echo "About to log in with credentialsId=dockerHubCredential for user ${dockerhubuser}"
   withCredentials([usernamePassword(credentialId:'dockerHubCredential', usernameVariable:'dockerUSER',passwordVariable:'dockerPASS')]){
     sh "docker login -u ${dockerUSER} -p ${dockerPASS}"
   }
